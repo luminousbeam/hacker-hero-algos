c1e79c58d7d357d5398563b9f9ea46facb09948f
luminousbeam <jocelyn.jeriah@gmail.com>
2020-06-16 18:50:41 -0400

    adding algo 16

---
+++ intermediate/algo-16.js
@@ -0,0 +1,17 @@
+// Array: Reverse
+
+// Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working ‘in-place’ means that you cannot use a second array – move values within the array that you are given.
+
+// Test Cases
+
+function reverse(arr) {}
+
+reverse();
+
+// reverse([1,3,5]) to return [5,3,1]
+
+// reverse([-1, 3, -5]) to return [-5,3,-1]
+
+// reverse([1, 2, 3, 4, 5]) to return [5,4,3,2,1]
+
+// reverse([2, 10, 13, 15]) to return [15,13,10,2]
